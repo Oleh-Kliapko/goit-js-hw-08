fd66c8b5f0cd3a2a4d3a1b07401610a541a99760
Oleh Kliapko <goldclub1977@gmail.com>
2022-10-29 10:15:26 +0300

    add Task #3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+  formEl: document.querySelector('.feedback-form'),
+  emailEl: document.querySelector('input'),
+  messageEl: document.querySelector('textarea'),
+};
+
+getFormOutput();
+
+refs.formEl.addEventListener('input', throttle(onSaveInputData, 500));
+refs.formEl.addEventListener('submit', onSubmitForm);
+
+/** functions */
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onSaveInputData(evt) {
+  const userMessage = evt.target.value;
+  const userEmail = evt.target.name;
+  formData[userEmail] = userMessage;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function getFormOutput(evt) {
+  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedFormData) {
+    refs.emailEl.value = savedFormData.email;
+    refs.messageEl.value = savedFormData.message;
+  }
+}
