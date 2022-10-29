648e4ce9fe7ff545954f21b072429a243e10fc66
Oleh Kliapko <goldclub1977@gmail.com>
2022-10-29 13:21:24 +0300

    bugfix Task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,6 +5,7 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const STORAGE_KEY = 'videoplayer-current-time';
+
 const savedOnPlayPosition = localStorage.getItem(STORAGE_KEY);
 const parsedOnPlayPosition = JSON.parse(savedOnPlayPosition);
 
@@ -20,18 +21,20 @@ const onPlay = evt => {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player
-  .setCurrentTime(parsedOnPlayPosition.seconds)
-  .then(function (seconds) {
-    seconds = parsedOnPlayPosition.seconds;
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        'the time is less than 0 or greater than the videos duration';
-        break;
-      default:
-        'other error';
-        break;
-    }
-  });
+if (savedOnPlayPosition) {
+  player
+    .setCurrentTime(parsedOnPlayPosition.seconds)
+    .then(function (seconds) {
+      seconds = parsedOnPlayPosition.seconds;
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          'the time is less than 0 or greater than the videos duration';
+          break;
+        default:
+          'other error';
+          break;
+      }
+    });
+}
